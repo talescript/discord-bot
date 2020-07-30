2238ee6921f55997555d238b5e53eb2addb6e28e
Alexander <teichopsia@gmail.com>
2020-07-29 21:28:58 -0400

    Add __pycache__/. Add a few functions

--- kevin_discord_bot.py
+++ kevin_discord_bot.py
@@ -1,6 +1,5 @@
-import asyncio
-import time
 import discord
+
 import config
 
 """
@@ -8,6 +7,11 @@ import config
     Click on the Server (top left) -> Server settings -> Widget
                                    -> Server ID
 """
+CSS_GRID = "https://developer.mozilla.org/en-US/docs/Web/CSS/grid"
+CSS_FLEX = "https://developer.mozilla.org/en-US/docs/Web/CSS/flex"
+KEVIN = "https://www.kevinpowell.co/"
+
+
 
 client = discord.Client()
 
@@ -36,11 +40,26 @@ async def on_message(message):
     """ Commands can only be run in the following channels
     """
     server_id = client.get_guild(config.SERVER_ID)
-    channels = ["commands"]
+    channels = ["off-topic"]
+    restricted_words = []
 
     if message.author == client.user:
         return
+
+    for word in restricted_words:
+        if message.content.count(word) > 0:
+            #await message.channel.purge(limit=1)  ## needs more permissions
+            await message.channel.send("Clean up your language you dirty rotten scoundrel")
     
+    if message.content == "!help":
+        embed = discord.Embed(title="Bot commands", description="If you press my buttons in the right order I will help you")
+        embed.add_field(name="!kevin", value="Shows kevin's website")
+        embed.add_field(name="!grid", value="MDN Docs on grid")
+        embed.add_field(name="!flex", value="MDN Docs on flex")
+        embed.add_field(name="!users", value="It tells you how many users are in the server.")
+
+        await message.channel.send(content=None, embed=embed)
+
     if str(message.channel) in channels:
 
         if message.content.find("alright") != -1:
@@ -48,9 +67,16 @@ async def on_message(message):
 
         elif message.content == "!users":
             await message.channel.send(f"Number of users {server_id.member_count}")
+        elif message.content == "!grid":
+            await message.channel.send(f"{CSS_GRID}") 
+        elif message.content == "!flex":
+            await message.channel.send(f"{CSS_FLEX}")
+        elif message.content == "!kevin":
+            await message.channel.send(f"{KEVIN}")
 
     else:
-        print(f"User {message.autor}... commands can not be ran in this channel")
+        # await message.channel.send(f"You can head over to #off-topic to issue commands") 
+        print(f"User {message.author}... commands can not be ran in this channel")
 
 
 
