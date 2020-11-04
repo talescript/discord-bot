233d54c758098fb1107d6aa5dd2be20a206c1bb9
Alexander <hello.talescript@gmail.com>
2020-11-04 19:01:54 -0400

    Formatting issues

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
-    
+
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def unban(ctx, *, member):
