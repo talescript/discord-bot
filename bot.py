83d687f0d1cf99d20c64891a459fa537a26d920a
Alexander <hello.talescript@gmail.com>
2020-11-09 02:18:44 -0400

    Infinite loop removal

--- bot.py
+++ bot.py
@@ -57,7 +57,6 @@ async def clear(ctx, amount=2):
 
 @client.listen('on_message')
 async def on_message(message):
-    await message.channel.send("working partially")
     reply_ext = ['html']
     reply = (
             f'"It\'s quite possible this asteroid is not entirely stable".'
